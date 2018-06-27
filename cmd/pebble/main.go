03c9ed8476f5d8e8ab09e768943b7d4e5f1a4eba
Ludovic Fernandez <ldez@users.noreply.github.com>
2018-06-27 20:36:05 +0200

    Feature: Custom DNS Server Address (#139)
    Add a way to add a custom DNS server address:
    - add a flag `dnsserver` 
    
    This allow to use custom port and custom address, ex:
    - `192.168.0.56:5053`
    - `8.8.8.8:53`
    
    Related to #33

--- cmd/pebble/main.go
+++ cmd/pebble/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
+	"net"
 	"net/http"
 	"os"
 
@@ -33,6 +35,10 @@ func main() {
 		"strict",
 		false,
 		"Enable strict mode to test upcoming API breaking changes")
+	resolverAddress := flag.String(
+		"dnsserver",
+		"",
+		"Define a custom DNS server address (ex: 192.168.0.56:5053 or 8.8.8.8:53).")
 	flag.Parse()
 	if *configFile == "" {
 		flag.Usage()
@@ -46,7 +52,11 @@ func main() {
 	err := cmd.ReadConfigFile(*configFile, &c)
 	cmd.FailOnError(err, "Reading JSON config file into config structure")
 
-	clk := clock.Default()
+	if len(*resolverAddress) > 0 {
+		setupCustomDNSResolver(*resolverAddress)
+	}
+
+	clk := clock.New()
 	db := db.NewMemoryStore(clk)
 	ca := ca.New(logger, db)
 	va := va.New(logger, clk, c.Pebble.HTTPPort, c.Pebble.TLSPort)
@@ -61,4 +71,14 @@ func main() {
 		c.Pebble.PrivateKey,
 		muxHandler)
 	cmd.FailOnError(err, "Calling ListenAndServeTLS()")
+}
+
+func setupCustomDNSResolver(dnsResolverAddress string) {
+	net.DefaultResolver = &net.Resolver{
+		PreferGo: true,
+		Dial: func(ctx context.Context, _, _ string) (net.Conn, error) {
+			d := net.Dialer{}
+			return d.DialContext(ctx, "udp", dnsResolverAddress)
+		},
+	}
 }
