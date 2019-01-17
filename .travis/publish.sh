70498736f1de9cb53efa48ba9d99a6f1b037d6cc
Daniel <cpu@letsencrypt.org>
2019-01-17 11:57:12 -0500

    CI: publish.sh shellcheck fixes

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -8,26 +8,26 @@ else
     exit 0
 fi
 
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
 BASE_NAMES=(pebble pebble-challtestsrv)
-for BASE_NAME in ${BASE_NAMES[@]}; do
+for BASE_NAME in "${BASE_NAMES[@]}"; do
     IMAGE_NAME="letsencrypt/${BASE_NAME}"
 
     echo "Updating docker ${IMAGE_NAME} image..."
 
     # create docker image
-    docker build -t ${IMAGE_NAME}:temp -f docker/${BASE_NAME}/Dockerfile .
+    docker build -t "${IMAGE_NAME}:temp" -f "docker/${BASE_NAME}/Dockerfile" .
 
     # push images
     if [[ -n "${TRAVIS_TAG}" ]]; then
         echo "Try to publish image: ${IMAGE_NAME}:${TRAVIS_TAG}"
-        docker tag ${IMAGE_NAME}:temp ${IMAGE_NAME}:${TRAVIS_TAG}
-        docker push ${IMAGE_NAME}:${TRAVIS_TAG}
+        docker tag "${IMAGE_NAME}:temp" "${IMAGE_NAME}:${TRAVIS_TAG}"
+        docker push "${IMAGE_NAME}:${TRAVIS_TAG}"
 
         echo "Try to publish image: ${IMAGE_NAME}:latest"
-        docker tag ${IMAGE_NAME}:${TRAVIS_TAG} ${IMAGE_NAME}:latest
-        docker push ${IMAGE_NAME}:latest
+        docker tag "${IMAGE_NAME}:${TRAVIS_TAG}" "${IMAGE_NAME}:latest"
+        docker push "${IMAGE_NAME}:latest"
     fi
 done
 
