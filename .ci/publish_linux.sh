42c87a6eca7da3f2200f92976c364f2f418a3bd5
Adrien Ferrand <ferrand.ad@gmail.com>
2019-02-22 18:07:11 +0100

    Move deploy script in new location. Remove unecessary tag control in linux deploy script, as it is controlled in Travis.

--- .ci/publish_linux.sh
+++ .ci/publish_linux.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    echo "Publishing..."
-else
-    echo "Skipping publishing"
-    exit 0
-fi
-
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
 BASE_NAMES=(pebble pebble-challtestsrv)
 for BASE_NAME in "${BASE_NAMES[@]}"; do
-    IMAGE_NAME="letsencrypt/${BASE_NAME}"
+    IMAGE_NAME="adferrand/${BASE_NAME}"
 
     echo "Updating docker ${IMAGE_NAME} image..."
 
     # create docker image
-    docker build -t "${IMAGE_NAME}:temp" -f "docker/${BASE_NAME}/Dockerfile" .
+    docker build -t "${IMAGE_NAME}:temp" -f "docker/${BASE_NAME}/Dockerfile-linux" .
 
     # push images
     if [ -n "${TRAVIS_TAG}" ]; then
@@ -25,7 +18,7 @@ for BASE_NAME in "${BASE_NAMES[@]}"; do
         docker tag "${IMAGE_NAME}:temp" "${IMAGE_NAME}:${TRAVIS_TAG}"
         docker push "${IMAGE_NAME}:${TRAVIS_TAG}"
 
-        echo "Try to publish image: ${IMAGE_NAME}:latest"
+        echo "Try to publish rolling image: ${IMAGE_NAME}:latest"
         docker tag "${IMAGE_NAME}:${TRAVIS_TAG}" "${IMAGE_NAME}:latest"
         docker push "${IMAGE_NAME}:latest"
     fi
