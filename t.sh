2e8594235f779a9db75128d7db06cb048101dd07
Shiloh Heurich <sheurich@fastly.com>
2024-02-23 14:26:03 -0500

    add poc for docker-go tests

--- t.sh
+++ t.sh
@@ -6,6 +6,14 @@ set -eu
 # Self-check with shellcheck
 shellcheck "$0"
 
+# Run the Go tests in a container
+# Building for race tests requires cgo
+docker run --rm -e CGO_ENABLED=1 \
+    --volume "$(pwd):/go/src/github.com/letsencrypt/pebble" \
+    --workdir /go/src/github.com/letsencrypt/pebble \
+    golang:1-alpine \
+    sh -c "apk add build-base && go test -v -race ./..."
+
 # Run the integration tests in containers
 docker compose \
     --file docker-compose.yml \
