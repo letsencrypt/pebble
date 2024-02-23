672612e90efc80f7c8ebfdd87d2807ecb771624b
Shiloh Heurich <sheurich@fastly.com>
2024-02-23 14:25:29 -0500

    move go mod check to lint from build

--- b.sh
+++ b.sh
@@ -1,19 +1,7 @@
-#!/bin/sh -x
+#!/bin/sh
 
 # Fail on error, undefined, and uninitialized variables
 set -eu
-
-# Disable GOWORK for local module maintenance
-export GOWORK=off
-
-# Tidy up the go.mod file and download/vendor dependencies
-# Check for changes to the go.mod and go.sum files and vendor directory
-go mod tidy
-git diff --exit-code go.mod
-git diff --exit-code go.sum
-go mod download
-go mod vendor
-git diff --exit-code vendor
 
 # Build the project
 go build -v -mod=vendor ./...
--- l.sh
+++ l.sh
@@ -6,5 +6,16 @@ set -eu
 # Self-check with shellcheck
 shellcheck "$0"
 
-# Run golangci-lint but ignore go.work files
-GOWORK=off golangci-lint run
+# Disable GOWORK for local-only module maintenance
+export GOWORK=off
+
+# Tidy up the go.mod file and download/vendor dependencies.
+# Check for changes to the go.mod and go.sum files and vendor directory.
+go mod tidy -v -x
+git diff --exit-code go.mod
+git diff --exit-code go.sum
+go mod vendor -v
+git diff --exit-code vendor
+
+# Run golangci-lint
+golangci-lint run
